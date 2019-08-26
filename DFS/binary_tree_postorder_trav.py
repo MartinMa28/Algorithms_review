8450588292852393090bcdb1bf5160ec01d6ff5d
MartinMa28 <1135485226@qq.com>
2019-08-26 10:16:06 -0700

    review iterative preorder & postorder trav

--- DFS/binary_tree_postorder_trav.py
+++ DFS/binary_tree_postorder_trav.py
@@ -9,11 +9,38 @@ class Solution:
     def __init__(self):
         self.trav = []
 
-    def _postorder_traversal(self, root: TreeNode) -> None:
+    def _postorder_traversal_recursive(self, root: TreeNode) -> None:
         if root:
             self._postorder_traversal(root.left)
             self._postorder_traversal(root.right)
             self.trav.append(root.val)
+
+    def _postorder_traversal(self, root: TreeNode) -> list:
+        stack = []
+        traversal = []
+
+        if root == None:
+            return []
+
+        # Postorder traversal is equal to the reversed "root, right, left" traversal, which
+        # is a variation of preorder traversal
+        stack.append(root)
+
+        while len(stack) > 0:
+            node = stack.pop()
+            traversal.append(node.val)
+            # Traverse following the order of root, right and left, so push left before right child.
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        # reverse traversal
+        for i in range(len(traversal) // 2):
+            traversal[i], traversal[len(traversal) - 1 - i]  = traversal[len(traversal) - 1 - i], traversal[i]
+
+        return traversal
+
 
     def postorderTraversal(self, root: TreeNode) -> None:
         self._postorder_traversal(root)
--- DFS/binary_tree_preorder_trav.py
+++ DFS/binary_tree_preorder_trav.py
@@ -9,11 +9,32 @@ class Solution:
     def __init__(self):
         self.trav = []
 
-    def _preorder_traversal(self, root: TreeNode) -> None:
+    def _preorder_traversal_recursive(self, root: TreeNode) -> None:
         if root:
             self.trav.append(root.val)
             self._preorder_traversal(root.left)
             self._preorder_traversal(root.right)
+
+    def _preorder_traversal(self, root: TreeNode) -> list:
+        stack = []
+        traversal = []
+
+        if root == None:
+            return []
+        
+        stack.append(root)
+
+        while len(stack) > 0:
+            popped = stack.pop()
+            traversal.append(popped.val)
+            # Preorder traversal follows the order of root, left and right, so
+            # push the right child before the left child into stack.
+            if popped.right:
+                stack.append(popped.right)
+            if popped.left:
+                stack.append(popped.left)
+
+        return traversal
 
     def preorderTraversal(self, root: TreeNode) -> list:
         self._preorder_traversal(root)
