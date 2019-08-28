82dcc7cf5d58ee59be2477efb33890470bc83c4e
MartinMa28 <1135485226@qq.com>
2019-08-27 21:36:57 -0700

    count the num of islands iteratively and recursively

--- DFS/0200_number_of_islands.py
+++ DFS/0200_number_of_islands.py
@@ -4,30 +4,61 @@ class Soluton:
             return 0
         
         stack = []
-        visited = []
         count = 0
         directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
 
-        stack.append((0, 0))
+        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == 1 and (i, j) not in visited:
+                if grid[i][j] == '1':
                     count += 1
+                    stack.append((i, j))
                 
                 while len(stack) > 0:
                     vtx_coor = stack.pop()
-                    visited.append(vtx_coor)
+                    grid[vtx_coor[0]][vtx_coor[1]] = '#'
 
                     for d in directions:
                         next_row = vtx_coor[0] + d[0]
                         next_col = vtx_coor[1] + d[1]
 
-                        if grid[next_row][next_col] == 1 and (next_row, next_col) not in visited:
+                        if next_row < len(grid) \
+                            and next_row >= 0 \
+                            and next_col < len(grid[0]) \
+                            and next_col >= 0 \
+                            and grid[next_row][next_col] == '1':
                             stack.append((next_row, next_col))
 
         return count
 
+    
+    def numIslands_recursive(self, grid: list) -> int:
+        if grid == None or grid == [] or grid == [[]]:
+            return 0
+
+        count = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    count += 1
+                    self._dfs(grid, i, j)
+                
+        return count
+
+    def _dfs(self, grid, row, col) -> None:
+        grid[row][col] = '#'
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        for d in directions:
+            next_row = row + d[0]
+            next_col = col + d[1]
+            if next_row >= 0 \
+                and next_row < len(grid) \
+                and next_col >= 0 \
+                and next_col < len(grid[0]) \
+                and grid[next_row][next_col] == '1':
+                self._dfs(grid, next_row, next_col)
 
 if __name__ == "__main__":
     solu = Soluton()
-    print(solu.numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]))
+    print(solu.numIslands_recursive([["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]))
