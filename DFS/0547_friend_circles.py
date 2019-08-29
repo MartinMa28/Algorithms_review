cce88fd1b8e4540bcafbdef8208a783086905ff6
MartinMa28 <1135485226@qq.com>
2019-08-29 14:33:33 -0700

    correct the DFS logic for friend circles

--- DFS/0547_friend_circles.py
+++ DFS/0547_friend_circles.py
@@ -1,13 +1,18 @@
 class Solution:
     def _dfs(self, M, row, col) -> None:
         M[row][col] = '#'
-        # Only needs to check in one direction because
-        # the matrix is symmetrical to the diagonal.
+        
+        # check friends horizontally
         for idx, candi in enumerate(M[row]):
             if candi == 1:
                 self._dfs(M, row, idx)
 
-    
+        # and then check vertically
+        for idx, candi in enumerate([r[col] for r in M]):
+            if candi == 1:
+                self._dfs(M, idx, col)
+
+
     def findCircleNum(self, M) -> int:
         if M == None or M == [] or M == [[]]:
             return 0
@@ -16,10 +21,14 @@ class Solution:
         n = len(M)
 
         for i in range(n):
-            for j in range(i, n):
+            for j in range(n):
                 if M[i][j] == 1:
                     count += 1
                     self._dfs(M, i, j)
 
         return count
         
+
+if __name__ == '__main__':
+    solu = Solution()
+    print(solu.findCircleNum([[1,1,0],[1,1,0],[0,0,1]]))
