6f188381af3b8e95b159790d8b3d2fd35aa5218d
MartinMa28 <1135485226@qq.com>
2019-09-24 01:30:34 -0700

    implement quick sort finally

---
+++ sortings/0912_merge_sort_bottom_up.py
@@ -0,0 +1,68 @@
+class Solution:
+    def _merge(self, nums1, nums2) -> list:
+        if nums1 == []:
+            return nums2
+        
+        if nums2 == []:
+            return nums1
+
+        if nums1[0] <= nums2[0]:
+            return [nums1[0]] + self._merge(nums1[1:], nums2)
+        else:
+            return [nums2[0]] + self._merge(nums1, nums2[1:])
+
+
+    def sortArray_merge_sort(self, nums: list) -> list:
+        if len(nums) <= 1:
+            return nums
+        
+        mid = len(nums) // 2
+
+        return self._merge(self.sortArray_merge_sort(nums[:mid]), self.sortArray_merge_sort(nums[mid:]))
+
+    
+    def _partition(self, nums: list) -> int:
+        """
+        Does the parition in place, and returns the true index of pivot.
+        """
+        pivot = nums[0]
+
+        # i indicates the right end of the smaller part.
+        # It begins at the right end of the array.
+        i = len(nums)
+
+        # j is the cursor scans the array from right to left, finding the number
+        # that is larger than pivot.
+        for j in range(len(nums) - 1, 0, -1):
+            if nums[j] > pivot:
+                i -= 1
+                nums[j], nums[i] = nums[i], nums[j]
+        
+        i -= 1
+        nums[0], nums[i] = nums[i], nums[0]
+
+        return i
+
+
+    
+    def sortArray(self, nums: list) -> list:
+        if len(nums) == 1:
+            return nums
+
+        pivot_idx = self._partition(nums)
+
+        if len(nums[:pivot_idx]) > 0:
+            nums[:pivot_idx] = self.sortArray(nums[:pivot_idx])
+
+        if len(nums[pivot_idx + 1:]) > 0:
+            nums[pivot_idx + 1:] = self.sortArray(nums[pivot_idx + 1:])
+
+        return nums[:pivot_idx] + [nums[pivot_idx]] + nums[pivot_idx + 1:]
+
+
+if __name__ == "__main__":
+    solu = Solution()
+    #print(solu.sortArray([5, 2, 3, 1]))
+    arr = [3, 6, 4, 5, 1, 2, 10, -2, 9, -1]
+    solu.sortArray(arr)
+    print(arr)
