dd41a64c158a0148e688c2956820763c1b9bba55
MartinMa28 <1135485226@qq.com>
2019-10-15 16:33:15 -0700

    exchange coins with bottom up DP solution

--- dynamic_programming/0322_coin_exchange.py
+++ dynamic_programming/0322_coin_exchange.py
@@ -26,9 +26,28 @@ class Solution:
         
       
 
-    def coinChange(self, coins: list, amount: int) -> int:
+    def coinChange_top_down_memoizing(self, coins: list, amount: int) -> int:
         # Sort the coins in the descending order
         if amount == 0:
             return 0
         
         return self._recursive_exchange(coins, amount)
+
+    
+    def coinChange(self, coins: list, amount: int) -> int:
+        if amount == 0:
+            return 0
+        
+        l = [-1 for _ in range(amount)]
+        l = [0] + l
+        
+        for i in range(1, amount + 1):
+            candidates = []
+            for c in coins:
+                if c <= i and l[i - c] != -1:
+                    candidates.append(l[i - c] + 1)
+            
+            if len(candidates) > 0:
+                l[i] = min(candidates)
+            
+        return l[-1]
