6b72154c3797d9553f0891503ff28cfb9b4f2ab0
MartinMa28 <1135485226@qq.com>
2019-10-16 12:08:07 -0700

    Solve n queens using backtracking

--- backtracking/0051_n_queens.py
+++ backtracking/0051_n_queens.py
@@ -1,70 +1,75 @@
-from copy import deepcopy
-
 class Solution:
+    def __init__(self):
+        self.results = []
+    
     def _is_safe(self, row, col) -> bool:
-        # check this row on the left side
-        for i in range(col):
-            if self.board[row][i] == 1:
-                return False
+        if sum(self.board[row]) > 0:
+            return False
+        
+        if sum([r[col] for r in self.board]) > 0:
+            return False
 
-        # check the upper diagonal on the left side
         for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
             if self.board[i][j] == 1:
                 return False
 
-        # check the lower diagonal on the left side
-        for i, j in zip(range(row, self.size), range(col, -1, -1)):
+        for i,j in zip(range(row, len(self.board)), range(col, len(self.board))):
             if self.board[i][j] == 1:
                 return False
-        
+
+        for i, j in zip(range(row, len(self.board)), range(col, -1, -1)):
+            if self.board[i][j] == 1:
+                return False
+
+        for i,j in zip(range(row, -1, -1), range(col, len(self.board))):
+            if self.board[i][j] == 1:
+                return False
+
         return True
-                
 
-    def _recursive_util(self, col) -> bool:
-        if col == self.size:
-            self.results.append(deepcopy(self.board))
-            return True
-        
-        res = False
-        for row in range(self.size):
-            if self._is_safe(row, col):     
-                # In current column, found a row to insert.
-                self.board[row][col] = 1
-                res = self._recursive_util(col + 1) or res
-                # backtracking
-                self.board[row][col] = 0
-                # if self._recursive_util(col + 1):
-                #     return True
-                # else:
-                #     # backtracking
-                #     self.board[row][col] = 0
+    
+    def _place_queen(self, row, col):
+        self.board[row][col] = 1
 
-        return res
+    
+    def _remove_queen(self, row, col):
+        self.board[row][col] = 0
 
+    def _backtracking(self, row):
+        if row == len(self.board):
+            self.results.append(self._str_transfer())
+            return
 
-    def solveNQueens(self, n: int) -> list:
-        self.size = n
-        self.board = [[0 for _ in range(n)] for _ in range(n)]
-        self.results = []
-
-        self._recursive_util(0)
-        return self._str_transfer()
+        for col in range(len(self.board[0])):
+            if self._is_safe(row, col):
+                self._place_queen(row, col)
+                self._backtracking(row + 1)
+                self._remove_queen(row, col)
 
 
     def _str_transfer(self) -> list:
-        str_results = []
-        for res in self.results:
-            r = []
-            for row in res:
-                s = ''
-                for num in row:
-                    s += '.' if num == 0 else 'Q'
-                r.append(s)
-            str_results.append(r)
-        
-        return str_results
+        ret = []
+        for row in self.board:
+            s = ''
+            for n in row:
+                if n == 0:
+                    s += '.'
+                else:
+                    s += 'Q'
+            
+            ret.append(s)
 
+        return ret
+
+    def solveNQueens(self, n: int) -> list:
+        self.board = [[0 for _ in range(n)] for _ in range(n)]
+
+        self._backtracking(0)
+
+        return self.results
+        
 
 if __name__ == "__main__":
     solu = Solution()
-    print(solu.solveNQueens(4))
+    print(solu.solveNQueens(8))
+    print(solu.results)
