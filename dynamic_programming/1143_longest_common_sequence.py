e1cd9d6d3595a66884374ec8105a4cd0adae19fe
MartinMa28 <1135485226@qq.com>
2019-10-22 02:02:55 -0700

    do more dp questions

--- dynamic_programming/1143_longest_common_sequence.py
+++ dynamic_programming/1143_longest_common_sequence.py
@@ -3,26 +3,23 @@ class Solution:
         m = len(text1)
         n = len(text2)
         
-        dists = [[0 for _ in range(n)] for _ in range(m)]
+        # Entries of the first row and col are equal to 0, because every string only has
+        # 0 LCS with an empty string.
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
         
-        for i in range(m):
-            for j in range(n):
-                if text1[i] == text2[j]:
-                    if i > 0 and j > 0:
-                        dists[i][j] = dists[i - 1][j - 1] + 1
-                    else:
-                        dists[i][j] = 1
+        # i denotes the i-th character in text1, j denotes the j-th character in text2
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    # If the trailing character is the same,
+                    # LCS(text1[:i], text2[:j]) == 1 + LCS(text1[:i - 1], text2[:j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1
                 else:
-                    if i > 0 and j > 0:
-                        dists[i][j] = max((dists[i][j - 1], dists[i - 1][j]))
-                    elif i > 0 and j == 0:
-                        dists[i][j] = dists[i - 1][j]
-                    elif i == 0 and j > 0:
-                        dists[i][j] = dists[i][j - 1]
-                    else:
-                        dists[i][j] = 0
+                    # Otherwise LCS(text1[:i], text2[:j]) ==
+                    # max(LCS(text1[:i], text2[:j - 1]), LCS(text1[:i - 1], text2[:j]))
+                    dp[i][j] = max((dp[i][j - 1], dp[i - 1][j]))
         
-        return dists[m - 1][n - 1]
+        return dp[m][n]
 
 if __name__ == "__main__":
     solu = Solution()
