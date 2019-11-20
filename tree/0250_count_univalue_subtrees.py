b1c79e307a479cba640acb588c1fac504c98a6ac
Martin <1135485226@qq.com>
2019-11-20 15:31:44 -0800

    use bottom up dp to solve unique binary search tree

--- tree/0250_count_univalue_subtrees.py
+++ tree/0250_count_univalue_subtrees.py
@@ -62,6 +62,38 @@ class Solution:
         self.cnt += is_univalue
         return is_univalue
     
+    def _is_uni(self, root) -> bool:
+        if root.left == None and root.right == None:
+            # leaf node is univalue
+            self.uni_cnt += 1
+            return True
+        
+        elif root.right == None:
+            if self._is_uni(root.left) and root.val == root.left.val:
+                self.uni_cnt += 1
+                return True
+            else:
+                return False
+    
+        elif root.left == None:
+            if self._is_uni(root.right) and root.val == root.right.val:
+                self.uni_cnt += 1
+                return True
+            else:
+                return False
+            
+        else:
+            left_uni = self._is_uni(root.left)
+            right_uni = self._is_uni(root.right)
+            if left_uni and\
+                right_uni and\
+                root.val == root.left.val and\
+                root.val == root.right.val:
+                self.uni_cnt += 1
+                return True
+            else:
+                return False
+
     def countUnivalSubtrees(self, root: TreeNode) -> int:
         if root == None:
             return 0
