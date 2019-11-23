24552fde759eb2ef26c7feceb3bb60710b4c2467
Martin <1135485226@qq.com>
2019-11-22 20:36:01 -0800

    practice a few binary search questions

--- binary_search/0033_search_in_rotated_sorted_array.py
+++ binary_search/0033_search_in_rotated_sorted_array.py
@@ -1,49 +1,46 @@
 class Solution:
     
-    def _find_pivot(self, nums, left, right) -> int:
-        mid = (left + right) // 2
-        if nums[mid] < nums[left]:
-            return self._find_pivot(nums, left, mid)
-        elif nums[mid] > nums[right]:
-            return self._find_pivot(nums, mid + 1, right)
-        else:
-            return left
-
-    
-    def _bi_search(self, nums, target, left, right) -> int:
-        if left > right:
-            return -1
-        else:
-            mid = (left + right) // 2
+    def _binary_search(self, nums, left, right, target):
+        while left <= right:
+            mid = (left + right) >> 1
             if nums[mid] == target:
                 return mid
             elif nums[mid] > target:
-                return self._bi_search(nums, target, left, mid - 1)
+                right = mid - 1
             else:
-                return self._bi_search(nums, target, mid + 1, right)
-
+                left = mid + 1
+                
+        return -1
     
-    def search(self, nums: list, target: int) -> int:
+    def search(self, nums: List[int], target: int) -> int:
         if nums == []:
             return -1
         
-        pivot = self._find_pivot(nums, 0, len(nums) - 1)
-
-        smaller_half = nums[pivot:]
-        larger_half = nums[:pivot]
-
-        if target >= smaller_half[0] and target <= smaller_half[-1]:
-            found_idx = self._bi_search(smaller_half, target, 0, len(smaller_half) - 1)
-            if found_idx >= 0:
-                return pivot + found_idx
+        if len(nums) == 1:
+            return 0 if nums[0] == target else -1
+        
+        left = 0
+        right = len(nums) - 1
+        
+        rotated_idx = -1
+        while left < right:
+            mid = (left + right) >> 1
+            if nums[mid] > nums[right]:
+                left = mid + 1
             else:
-                return -1
+                right = mid
+        
+        rotated_idx = left
+        
+        if rotated_idx == 0:
+            return self._binary_search(nums, 0, len(nums) - 1, target)
+        
+        if nums[0] <= target and nums[rotated_idx - 1] >= target:
+            return self._binary_search(nums, 0, rotated_idx - 1, target)
+        elif nums[rotated_idx] <= target and nums[-1] >= target:
+            return self._binary_search(nums, rotated_idx, len(nums) - 1, target)
         else:
-            found_idx = self._bi_search(larger_half, target, 0, len(larger_half) - 1)
-            if found_idx >= 0:
-                return found_idx
-            else:
-                return -1
+            return -1
 
 
 if __name__ == "__main__":
