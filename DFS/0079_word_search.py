019925a2866547be085d2504eb07dc45a8567be3
MartinMa28 <1135485226@qq.com>
2019-11-29 13:47:58 -0800

    solve the largest rectangle in histogram using stack in O(n)

--- DFS/0079_word_search.py
+++ DFS/0079_word_search.py
@@ -1,46 +1,47 @@
 class Solution:
     def __init__(self):
-        self.directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        self.cur_word = ''
+        self.directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        self.found = False
         self.visited = set()
+    
+    def _on_board(self, row, col):
+        return row >= 0 and row < len(self.board) \
+                and col >= 0 and col < len(self.board[0])
 
-
-    def _is_safe(self, row, col) -> bool:
-        if row >= 0 and row < len(self.board) \
-            and col >= 0 and col < len(self.board[0]) \
-            and (row, col) not in self.visited:
-            return True
-        else:
-            return False
-
-    def _dfs(self, row, col) -> bool:
-        self.cur_word += self.board[row][col]
-        self.visited.add((row, col))
-        if self.word[len(self.cur_word) - 1] == self.cur_word[-1]:
-            # If the added character is correct, keep 
-            # searching other characters or the word is complete.
-            if self.word == self.cur_word:
-                return True
-            else:
+    
+    def _dfs(self, row, col, idx) -> bool:
+        if self._on_board(row, col) and (row, col) not in self.visited:
+            if self.board[row][col] == self.word[idx]:
+                self.visited.add((row, col))
+                if idx == len(self.word) - 1:
+                    return True
+                
                 for d in self.directions:
-                    if self._is_safe(row + d[0], col + d[1]) \
-                        and self._dfs(row + d[0], col + d[1]):
+                    new_row = d[0] + row
+                    new_col = d[1] + col
+                    
+                    if self._dfs(new_row, new_col, idx + 1):
                         return True
-
-        # No matter this character is not correct or this character
-        # doesn't lead to the complete word, backtrack over here.
-        self.cur_word = self.cur_word[:-1]
-        self.visited.discard((row, col))
-
+                
+                # backtracking
+                self.visited.discard((row, col))
+        
         return False
-            
+                        
+    
     def exist(self, board: list, word: list) -> bool:
+        if board == None or board == [] or board == [[]]:
+            return False
+        
         self.board = board
         self.word = word
-
-        for i in range(len(self.board)):
-            for j in range(len(self.board[0])):
-                if self._dfs(i, j):
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self._dfs(i, j, 0):
                     return True
-
+                
         return False
+if __name__ == "__main__":
+    solu = Solution()
+    print(solu.exist([["a","a","a","a"],["a","a","a","a"],["a","a","a","a"]], "aaaaaaaaaaaaa"))
