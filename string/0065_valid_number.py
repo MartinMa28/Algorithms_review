e2a92854f0c69e1487b22fb27266c1650e4bed01
Martin <1135485226@qq.com>
2020-01-13 21:35:15 -0800

    practice more

--- string/0065_valid_number.py
+++ string/0065_valid_number.py
@@ -1,45 +1,52 @@
 class Solution:
-    def _is_decimal(self, s):
-        if not s:
-            return False
+    @staticmethod
+    def _is_integer(s) -> bool:
+        if s[0] in ('+', '-'):
+            s = s[1:]
         
-        if s[0] == '-' or s[0] == '+':
+        return s.isdecimal()
+    
+    @staticmethod
+    def _is_decimal(s) -> bool:
+        if s[0] in ('+', '-'):
             s = s[1:]
         
         if s.isdecimal():
             return True
         
-        s = s.split('.')
+        splitted_dot = s.split('.')
         
-        if len(s) == 2:
-            if s[0].isdecimal() and s[1].isdecimal():
-                return True
-            elif s[0] == '' and s[1].isdecimal():
-                return True
-            elif s[0].isdecimal() and s[1] == '':
-                return True
-            else:
-                return False
+        if len(splitted_dot) != 2:
+            return False
+        elif splitted_dot[0] and splitted_dot[1]:
+            return splitted_dot[0].isdecimal() and splitted_dot[1].isdecimal()
+        elif not splitted_dot[0] and splitted_dot[1]:
+            return splitted_dot[1].isdecimal()
+        elif splitted_dot[0] and not splitted_dot[1]:
+            return splitted_dot[0].isdecimal()
         else:
             return False
-    
-    def _is_integer(self, s):
-        if s:
-            if s[0] in ('-', '+'):
-                return s[1:].isdecimal()
-            else:
-                return s.isdecimal()
-        else:
-            return False
-    
+        
+            
     def isNumber(self, s: str) -> bool:
         s = s.strip()
         
-        if self._is_decimal(s):
-            return True
-        else:
-            s = s.split('e')
-            if len(s) == 2 and self._is_decimal(s[0]) and self._is_integer(s[1]):
-                return True
+        if not s:
+            return False
+        elif 'e' in s:    
+            splitted_e = s.split('e')
+            
+            if len(splitted_e) != 2:
+                return False
+            elif splitted_e[0] and splitted_e[1]:
+                return self._is_decimal(splitted_e[0]) and self._is_integer(splitted_e[1])
             else:
                 return False
+        else:
+            return self._is_decimal(s)
+
+def _is_integer(s_num):
+    pass
+
+def validate_num(s_num):
+    pass
